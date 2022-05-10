a6109c6721fdec2889542bff961c033d7388aac4
Alisher Mukashev <ali_2475@mail.ru>
2022-05-10 23:08:13 +0800

    Updates

---
+++ precache-manifest.baa1f4d0e9727365611eddadbba54de1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9fb7a616f1ba4de9d026de0ac19926c",
+    "url": "/age-gender-race/index.html"
+  },
+  {
+    "revision": "ea4949b7d9b6615590bf",
+    "url": "/age-gender-race/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "242bbc7f77e2d156f78a",
+    "url": "/age-gender-race/static/js/2.dfc5378f.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/age-gender-race/static/js/2.dfc5378f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4949b7d9b6615590bf",
+    "url": "/age-gender-race/static/js/main.c49d1c74.chunk.js"
+  },
+  {
+    "revision": "e7dbd57b166e2317874a",
+    "url": "/age-gender-race/static/js/runtime-main.a15d3f69.js"
+  }
+]);
